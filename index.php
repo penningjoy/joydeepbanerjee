3e6a015dbfd5502159e12893cda36fbe9d2308c3
Joydeep Banerjee <joydeepbanerjee2@gmail.com>
2018-01-09 13:51:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php include('index.html'); 
-
-function redirectTohttps() 
- { 
-   if($_SERVER['HTTPS']!=”on”) 
-   { 
-     $redirect= “https://”.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-     header(“Location:$redirect”);
-   } 
- } ?>
+?>
